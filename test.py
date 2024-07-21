1c3477b81432e6d380c95e3099db2a6b535e1149
Ahmad Naseh <anaseh@alarm.com>
2024-07-21 12:25:57 -0400

    Test image and python cache

--- test.py
+++ test.py
@@ -2,23 +2,21 @@ import cv2
  
 # To read image from disk, we use
 # cv2.imread function, in below method,
-img = cv2.imread("Media.jpg", cv2.IMREAD_GRAYSCALE)
-blur = cv2.GaussianBlur(img, (7,7), 0) 
+img = cv2.imread("handwritten.png", cv2.IMREAD_GRAYSCALE)
+im2 = cv2.imread("Media.jpg", cv2.IMREAD_GRAYSCALE)
+blur = cv2.GaussianBlur(img, (5,5), 0) 
 thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
-# contours, mat = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+contours, mat = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 # print(len(contours))
 
-cv2.imshow('Gaussian', thresh) 
+# cv2.imshow('Gaussian', mat) 
+
+cv2.drawContours(img, contours, 130, (0, 255, 0), 3)
+
+cv2.imshow('Gaussian', img)
 cv2.waitKey(0) 
-# Creating GUI window to display an image on screen
-# first Parameter is windows title (should be in string format)
-# Second Parameter is image array
-# cv2.imshow("image", thresh)
-
-  
-
-
  
 # To hold the window on screen, we use cv2.waitKey method
 # Once it detected the close input, it will release the control
@@ -26,7 +24,6 @@ cv2.waitKey(0)
 # First Parameter is for holding screen for specified milliseconds
 # It should be positive integer. If 0 pass an parameter, then it will
 # hold the screen until user close it.
-cv2.waitKey(0)
  
 # It is for removing/deleting created GUI window from screen
 # and memory
